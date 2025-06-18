c31864460a806008f94407ec9024185a5f5811f9
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2025-06-18 12:17:37 +0300

    Add react-router-dom for routing and refactor App component with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AnimatePresence, motion } from "framer-motion";
+import { AxeIcon, Home } from "lucide-react";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const location = useLocation();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Nav />
+			<AnimatePresence mode="wait">
+				<Routes location={location} key={location.pathname}>
+					<Route
+						path="/"
+						element={
+							<PageWrapper>
+								<Home className="text-gray-800 w-64 h-64" />
+							</PageWrapper>
+						}
+					/>
+					<Route
+						path="/about"
+						element={
+							<PageWrapper>
+								<AxeIcon className="text-gray-800 w-64 h-64" />
+							</PageWrapper>
+						}
+					/>
+				</Routes>
+			</AnimatePresence>
+		</>
+	);
 }
+export default App;
 
-export default App
+const PageWrapper = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<motion.div
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -20 }}
+			transition={{ duration: 0.5 }}
+			className="flex items-center justify-center h-screen"
+		>
+			{children}
+		</motion.div>
+	);
+};
+
+const Nav = () => {
+	return (
+		<nav className="bg-gray-800 backdrop-blur-md p-4 flex justify-between drop-shadow-amber-300 shadow-2xl z-50">
+			<div className="flex items-center justify-center gap-4 w-full">
+				<Link
+					to="/"
+					className="text-white text-2xl font-bold hover:text-gray-300"
+				>
+					Home
+				</Link>
+				<Link
+					to="/about"
+					className="text-white text-2xl font-bold hover:text-gray-300"
+				>
+					About
+				</Link>
+			</div>
+		</nav>
+	);
+};
